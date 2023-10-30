88e632a3b93a51a5f5b28c77d33bc1690083c187
ricky <39970880+torickyto@users.noreply.github.com>
2023-10-30 15:02:13 -0600

    Created Services.cs to hold methods. In Service.cs: -Grabbed flights from CSV. -Created a template for MakeReservation method.

---
+++ GroupFlightProject/Data/Services.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupFlightProject.Data
+{
+    public class Services
+    {
+        private List<Flight> flights;
+        public Services()
+        {
+            FlightCsvLoader flightLoader = new FlightCsvLoader();
+            flights = flightLoader.LoadFlights("GroupFlightProject/flights.csv").ToList();
+        }
+
+        public List<Flight> FindFlights(string departureLocation, string destination, string day)
+        {
+            return flights.Where (f => f.DepartureLocation.Equals(departureLocation, StringComparison.OrdinalIgnoreCase) && 
+            f.Destination.Equals(destination, StringComparison.OrdinalIgnoreCase) &&    
+            f.Day.Equals(day, StringComparison.OrdinalIgnoreCase))
+            .ToList();
+        }
+
+        public Reservation MakeReservation (Flight pickedFlight, string name, string citizenship)
+        {
+            if (pickedFlight == null || string.IsNullOrEmpty(name) || string.IsNullOrEmpty(citizenship))
+            {
+                throw new ArgumentException("Invalid input.");
+            }
+            if (pickedFlight.AvailableSeats > 0)
+            {
+                pickedFlight.AvailableSeats--;
+
+                Reservation newReservation = new Reservation
+                {
+                    //implement code generation method
+                    //flight code
+                    //airline
+                    //cost
+                    //name
+                    Citizenship = citizenship,
+                    IsActive = true
+                };
+                //save reservation method
+                return newReservation;
+            }
+            
+            else
+            {
+                throw new InvalidOperationException("No more seats availible.");
+            }
+        }
+
+        //
+    }
+}
