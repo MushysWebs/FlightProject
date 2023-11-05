3bab1c7af4584b882d96475be425292b7735b1b7
ricky <39970880+torickyto@users.noreply.github.com>
2023-11-05 16:38:04 -0700

    WORKING ON FIXING FIND FLIGHTS LOGIC

--- GroupFlightProject/Data/Services.cs
+++ GroupFlightProject/Data/Services.cs
@@ -6,37 +6,28 @@ using System.Threading.Tasks;
 
 namespace GroupFlightProject.Data
 {
-	
-	public class Services
+
+    public class Services
     {
         private List<Flight> flights;
-		private FlightCsvLoader flightLoader;
-		public Services()
-		{
-			flightLoader = new FlightCsvLoader();
-			LoadFlightsAsync();
-		}
+        private FlightCsvLoader flightLoader;
+        public Services()
+        {
+            flightLoader = new FlightCsvLoader();
+            LoadFlightsAsync();
+        }
 
-		internal class FlightCsvLoader
-		{
-			public List<Flight> LoadFlightsFromCsv(string csvContents)
-			{
-				var flights = new List<Flight>();
-				return flights;
-			}
-		}
+        private async Task LoadFlightsAsync()
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("flights.csv");
+            using var reader = new StreamReader(stream);
 
-		private async Task LoadFlightsAsync()
-		{
-			using var stream = await FileSystem.OpenAppPackageFileAsync("flights.csv");
-			using var reader = new StreamReader(stream);
+            var csvContents = await reader.ReadToEndAsync();
 
-			var csvContents = await reader.ReadToEndAsync();
+            flights = flightLoader.LoadFlightsFromCsv(csvContents);
+        }
 
-			flights = flightLoader.LoadFlightsFromCsv(csvContents);
-		}
-
-		public List<Flight> FindFlights(string departureLocation, string destination, string day)
+        public List<Flight> FindFlights(string departureLocation, string destination, string day)
         {
             return flights.Where(f => f.DepartureLocation.Equals(departureLocation, StringComparison.OrdinalIgnoreCase) &&
             f.Destination.Equals(destination, StringComparison.OrdinalIgnoreCase) &&
@@ -96,39 +87,39 @@ namespace GroupFlightProject.Data
             METHOD FOR MODIFYING RESERVATIONS
         }*/
     }
-	internal class FlightCsvLoader
-	{
-		public List<Flight> LoadFlightsFromCsv(string csvContents)
-		{
-			var flights = new List<Flight>();
+    public class FlightCsvLoader
+    {
+        public List<Flight> LoadFlightsFromCsv(string csvContents)
+        {
+            var flights = new List<Flight>();
 
-			string[] lines = csvContents.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            string[] lines = csvContents.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
 
-			foreach (string line in lines)
-			{
-				var values = line.Split(',');
+            foreach (string line in lines)
+            {
+                var values = line.Split(',');
 
-				if (values.Length >= 8)
-				{
-					Flight newFlight = new Flight
-					{
-						FlightCode = values[0],
-						Airline = values[1],
-						DepartureLocation = values[2],
-						Destination = values[3],
-						Day = values[4],
-						Time = values[5],
-						AvailableSeats = int.Parse(values[6]),
-						FlightNumber = int.Parse(values[7])
-					};
+                if (values.Length >= 8)
+                {
+                    Flight newFlight = new Flight
+                    {
+                        FlightCode = values[0],
+                        Airline = values[1],
+                        DepartureLocation = values[2],
+                        Destination = values[3],
+                        Day = values[4],
+                        Time = values[5],
+                        AvailableSeats = int.Parse(values[6]),
+                        FlightNumber = int.Parse(values[7])
+                    };
 
-					flights.Add(newFlight);
-				}
-			}
+                    flights.Add(newFlight);
+                }
+            }
 
-			return flights;
-		}
-	}
+            return flights;
+        }
+    }
 }
 
 
