812815fc32b3157a988fdd9265bc493039676ea1
ricky <39970880+torickyto@users.noreply.github.com>
2023-11-14 00:56:21 -0700

    Update ToDoLIST.cs

--- GroupFlightProject/Pages/ToDoLIST.cs
+++ GroupFlightProject/Pages/ToDoLIST.cs
@@ -1,35 +1,34 @@
-﻿/*					OOP 2 ASSIGNMENT TO DO LIST
+/*					OOP 2 ASSIGNMENT TO DO LIST
+
+note: LOW PRIORITY DOES NOT NEED TO BE DONE.
 
 ~~~~~~~~~~~~~~~~~~~~~EASY~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~												PRIORITY					APPROX. TIME
 FLIGHTS PAGE
-		-make sure reservation code is unique. check to see if it matches existing error code.			LOW							15 minutes
-		-clean up UI to resemble example screenshots (or nicer!)										LOW							1 hour
-		-update flight code generation to match LL-DDDD format											LOW							5 minutes
-		-create	ReservationManager class																LOW							30 minutes 
-			-move code generation logic to this class													LOW							FAST
+		-make sure reservation code is unique. check to see if it matches existing error code.							LOW						15 minutes
+		-clean up UI to resemble example screenshots (or nicer!)										LOW						3o minutes
+		-update flight code generation to match LL-DDDD format											MEDIUM						5 minutes
+
+	EITHER
+		-move ReservationManager class to its own .cs file											MEDIUM						15 minutes 
+			-incorporate ReservationManager.cs into Reservations.razor and Flights.razor							MEDIUM						15 minutes
+	OR
+		-copy ReservationManager class into Reservations.cs if you're a bitch									MEDIUM						1 minute
 
 RESERVATION PAGE
-		-create basic UI for reservation page															MEDIUM						30 minutes
-			-clean up UI to resemble example screenshots (or nicer!)									LOW							1 hour
-		
+		-clean up UI to resemble example screenshots (or nicer!)										LOW						30 minutes
 
-
-
-~~~~~~~~~~~~~~~~~~~~~~MID~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~		
+~~~~~~~~~~~~~~~~~~~~~~EASY BUT HARDER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~		
 FLIGHTS PAGE
-		-implement a method to modify existing reservations	(requires [PREREQ1])						MEDIUM						1 hour
-			-method can update the traveller’s name and citizenship										LOW							10 minutes		
-			-method can delete reservations, marking it inactive and freeing up a seat					LOW							10 minutes
-		-make sure the flight seat gets updated in the CVS file											MEDIUM						1 hour
+		-implement a method to modify existing reservations											HIGH						1 hour
+			-method can update the traveller’s name and citizenship										MEDIUM						10 minutes		
+			-method can delete reservations, marking it inactive and freeing up a seat							MEDIUM						10 minutes
 
 RESERVATION PAGE
-		-create findReservation method (requires [PREREQ1])												MEDIUM						30 minutes
-		-create updateReservation method (requires [PREREQ1])											MEDIUM
+		-create findReservation method														HIGH						30 minutes
+		-create updateReservation method													HIGH						30 minutes
 
 
-~~~~~~~~~~~~~~~~~~~~~HARD~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~		
-FLIGHTS PAGE
-		[PREREQ1]-serialize and save reservation. try storing it as a .json.							HIGH						1.5 hours
+
 																																	__________
-																																	6+ hours*/
+																																	3+ hours*/
 																																	
