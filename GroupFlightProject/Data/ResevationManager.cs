84316feca2964ca6bf47d6f9febc36e77124a2a6
ricky <39970880+torickyto@users.noreply.github.com>
2023-11-14 20:27:15 -0700

    ADDED COMMENTS TO RESERVATION MANAGER CS AND REMOVED  UNNESCARY FILES

--- GroupFlightProject/Data/ResevationManager.cs
+++ GroupFlightProject/Data/ResevationManager.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.Extensions.Logging;
+﻿/*  Names: Ricky To, Grady Spurril, Logan Hoppen
+    Date: November 14th 2023
+    Description:    This code file defines two classes, Reservation and ReservationManager, 
+                    used to manage flight reservations.
+                    Reservation class handles the data for a flight reservation.
+                    ReservationManager handles the loading, updating, and persisting of reservations in a JSON file.
+                    It provides functionality to find reservations based on search criteria, update reservation details,
+                    and generates reservation codes.
+ */
+
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
 
+// a single flight reservation with its details
 public class Reservation
 {
     public string ReservationCode { get; set; }
@@ -15,28 +26,35 @@ public class Reservation
     public string Citizenship { get; set; }
     public bool IsActive { get; set; }
 
+    // Property to get/set the status of the reservation as "Active" or "Inactive"
     public string Status
     {
         get => IsActive ? "Active" : "Inactive";
         set => IsActive = value == "Active";
     }
 
+    // Overrides ToString() to provide a string representation of the reservation
     public override string ToString()
     {
         return $"Reservation Code: {ReservationCode}, Flight: {FlightCode}, Airline: {Airline}, Cost: {Cost}, Name: {Name}, Citizenship: {Citizenship}, Status: {Status}";
     }
 }
+
+// Manages the reservations and interactions with the storage file.
 public class ReservationManager
 {
+    // List to hold all reservations
     public List<Reservation> Reservations { get; set; } = new List<Reservation>();
+    // File path to store reservations (Local Appdata)
     public string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Traveless", "reservations.json");
 
+    // initialize the ReservationManager and load existing reservations
     public ReservationManager()
     {
         // Ensure the directory exists
         Directory.CreateDirectory(Path.GetDirectoryName(filePath));
 
-        // Load existing reservations if the file exists
+        // Load existing reservations
         try
         {
             if (File.Exists(filePath))
@@ -50,28 +68,11 @@ public class ReservationManager
             Console.WriteLine($"No file found. {ex.Message}");
         }
     }
-    private void LoadReservations()
-    {
-        // Ensure the directory exists
-        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
 
-        // Load existing reservations if the file exists
-        try
-        {
-            if (File.Exists(filePath))
-            {
-                string json = File.ReadAllText(filePath);
-                Reservations = JsonSerializer.Deserialize<List<Reservation>>(json) ?? new List<Reservation>();
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Could not save file. {ex.Message}");
-        }
-    }
-
+    // Finds reservations based on the search criteria
     public List<Reservation> FindReservations(string reservationCode, string airline, string name)
     {
+        // Return all reservations if no criteria specified. otherwise, filter the list
         return string.IsNullOrWhiteSpace(reservationCode) &&
                string.IsNullOrWhiteSpace(airline) &&
                string.IsNullOrWhiteSpace(name)
@@ -83,7 +84,7 @@ public class ReservationManager
             ).ToList();
     }
 
-
+    // Updates a reservation with new details
     public void UpdateReservation(Reservation updatedReservation)
     {
         // Find and update the reservation in the list
@@ -96,12 +97,14 @@ public class ReservationManager
         }
     }
 
+    // Persists the current state of reservations to the JSON file
     public void Persist()
     {
         string json = JsonSerializer.Serialize(Reservations);
         File.WriteAllText(filePath, json);
     }
 
+    // Generates a new reservation code
     public string GenerateReservationCode()
     {
         const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
